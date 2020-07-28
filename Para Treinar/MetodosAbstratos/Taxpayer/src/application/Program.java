cb9bd20c93abc09a87eb6a500ca50022daf3760f
fhellipevalentin <fhellipereis@outlook.com>
2020-07-28 17:12:17 -0300

    metodos abstratos finalizados

--- Para Treinar/MetodosAbstratos/Taxpayer/src/application/Program.java
+++ Para Treinar/MetodosAbstratos/Taxpayer/src/application/Program.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Company;
 import entities.Individual;
 import entities.Taxpayer;
 
@@ -21,24 +22,36 @@ public class Program {
 		for (int i=1; i<=n; i++) {
 			System.out.println("Tax payer #" + i + " data:");
 			System.out.print("Individual or Company (i/c)? ");
-			sc.nextLine();
+			char type = sc.next().charAt(0);
 			System.out.print("Name: ");
+			sc.nextLine();
 			String name = sc.nextLine();
 			System.out.print("Anual income: ");
 			double anualIncome = sc.nextDouble();
-			char type = sc.next().charAt(0);
+			
 			if (type == 'i') {
 				System.out.print("Health Expenditures: ");
 				double healthExpenditures = sc.nextDouble();
 				Individual individual = new Individual(name, anualIncome, healthExpenditures);
-				list.add(individual);
+				list.add(individual);  
 			}
 			else {
 				System.out.print("Number Of Employees: ");
 				int numberOfEmployees = sc.nextInt();
+				Company company = new Company(name, anualIncome, numberOfEmployees);
+				list.add(company);
 			}
 		}
+		System.out.println();
+		System.out.println("TAXES PAID:"); 
+		for (Taxpayer taxpayer : list) {
+			
+			System.out.println(taxpayer.getName() + ": $ " + String.format("%.2f", taxpayer.tax()));
+		}
 		
+		//Alex: $ 11500.00 SoftTech: $ 56000.00 Bob: $ 29500.00
+		//TOTAL TAXES: $ 97000.00
+
 		
 		
 		sc.close();
